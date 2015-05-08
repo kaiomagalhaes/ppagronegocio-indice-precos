8b55b8cff0452e3beef3200c68683907fd1021d7
Kaio Magalhães <kaio.zeroglosa@gmail.com>
2015-05-08 18:23:44 -0300

    refactoring product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,35 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
- 
-  it "is valid when vulgarName is defined" do 
-  		category = Category.create(description:"test")
-  		product = Product.new(vulgarName:"test")
-  		product.category = category
+RSpec.describe Product, type: :model do	
+
+	let(:category) do 
+		Category.create(description:"test")
+	end
+
+	it "is valid when vulgarName is defined" do 
+		product = Product.new(vulgarName:"test")
+		product.category = category
 		expect(product).to be_valid
 	end
 
 	it "is invalid when vulgarName is not defined" do 
-		product = Product.new()
+		product = Product.new(category:category)
 		expect(product).to be_invalid
 	end
 
 	it "is invalid when already have a product with this cientificName defined" do 
-		Product.create(cientificName:"test")
+		Product.create(cientificName:"test",category:category)
 		product = Product.new(cientificName:"test")
 		expect(product).to be_invalid
 	end
 
 	it "is valid when have prices associated with him" do 
 		price = Price.create(value:0)
-		category = Category.create(description:"test")
 		product = Product.create(vulgarName:"test",category:category)
 		product.prices = [price]
 		expect(product.prices.first).to eql(price)
 	end
 
 	it "is valid when have category associated with him" do 
-		category = Category.create(description:"test")
 		product = Product.create(vulgarName:"test",category:category)
 		expect(product.category).to eql(category)
 	end
