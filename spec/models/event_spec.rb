155ddb209a313075f481ead7c89df766d478c1c3
Kaio Magalhães <kaio.zeroglosa@gmail.com>
2015-05-08 18:40:59 -0300

    refactoring event_spec_test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,25 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
- 
-  it "is valid when description is defined" do 
-		event = Event.new(description:"test")
+
+	let(:event) do 
+		Event.create(description:"test")
+	end
+
+	it "is valid when description is defined" do 
 		expect(event).to be_valid
 	end
 
 	it "is invalid when description is not defined" do 
-		event = Event.new()
-		expect(event).to be_invalid
+		invalid_event = Event.new()
+		expect(invalid_event).to be_invalid
 	end
 
 	it "is invalid when already have a event with this description defined" do 
 		Event.create(description:"test")
-		event = Event.new(description:"test")
-		expect(event).to be_invalid
+		invalid_event = Event.create(description:"test")
+		expect(invalid_event).to be_invalid
 	end
 
 	it "is valid when have prices associated with him" do 
-		event = Event.create(description:"test")
 		price = Price.create(value:28)
 		event.prices = [price]
 		expect(price.events.first).to eql(event)
