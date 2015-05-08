d5be2eba2496c74c56486b1ff7cec289285edbbf
Kaio Magalhães <kaio.zeroglosa@gmail.com>
2015-05-08 18:33:36 -0300

    refactoring price_spec

--- spec/models/price_spec.rb
+++ spec/models/price_spec.rb
@@ -1,26 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Price, type: :model do
-  
+
+	let(:price) do 
+		Price.new(value:28)
+	end
+
 	it "is valid when value is defined" do 
-		price = Price.new(value:"test")
 		expect(price).to be_valid
 	end
 
 	it "is invalid when value is not defined" do 
-		price = Price.new()
-		expect(price).to be_invalid
+		invalid_price = Price.new()
+		expect(invalid_price).to be_invalid
 	end
 
 	it "is valid when have events associated with him" do 
-		price = Price.create(value:28)
 		event = Event.create(description:"test")
 		price.events = [event]
 		expect(price.events.first).to eql(event)
 	end
 
 	it "is valid when have products associated with him" do 
-		price = Price.create(value:0)
 		category = Category.create(description:"test")
 		product = Product.create(vulgarName:"test",category:category)
 		price.products = [product]
